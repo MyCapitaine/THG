cf3512713cecd38e41882af9e1598e3d2c9adfc7
MyCapitaine <1051943988@qq.com>
2015-07-26 20:51:59 +0800

    完整的项目

--- core/src/org/thg/ui/gallery/Music_part.java
+++ core/src/org/thg/ui/gallery/Music_part.java
@@ -1,7 +1,12 @@
 package org.thg.ui.gallery;
 
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.scenes.scene2d.Group;
 
 public class Music_part extends Group {
-
+	public static Music music;
+	
+	
+	
+	
 }
